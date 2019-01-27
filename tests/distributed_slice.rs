b72f86d792ae545faca12272536c5c8d6698a1cc
David Tolnay <dtolnay@gmail.com>
2019-01-26 17:13:10 -0800

    Add basic distributed slice unit test

---
+++ tests/distributed_slice.rs
@@ -0,0 +1,25 @@
+use linkme::distributed_slice;
+
+#[distributed_slice]
+static SHENANIGANS: [i32] = [..];
+
+#[distributed_slice(SHENANIGANS)]
+static N: i32 = 9;
+
+#[distributed_slice(SHENANIGANS)]
+static NN: i32 = 99;
+
+#[distributed_slice(SHENANIGANS)]
+static NNN: i32 = 999;
+
+#[test]
+fn test() {
+    assert_eq!(SHENANIGANS.len(), 3);
+
+    let mut sum = 0;
+    for n in SHENANIGANS {
+        sum += n;
+    }
+
+    assert_eq!(sum, 9 + 99 + 999);
+}
