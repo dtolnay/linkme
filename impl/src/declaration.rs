4388d8e1715517081ae0c5e7f2f5cd680e7c6397
David Tolnay <dtolnay@gmail.com>
2019-08-17 17:03:50 -0700

    Cut down some unneeded cfgs

--- impl/src/declaration.rs
+++ impl/src/declaration.rs
@@ -70,28 +70,27 @@ pub fn expand(input: TokenStream) -> TokenStream {
     TokenStream::from(quote! {
         #(#attrs)*
         #vis static #ident: linkme::DistributedSlice<#ty> = {
+            #[cfg(any(target_os = "linux", target_os = "macos"))]
             extern "C" {
                 #[cfg_attr(target_os = "linux", link_name = #linux_section_start)]
                 #[cfg_attr(target_os = "macos", link_name = #macos_section_start)]
-                #[cfg(any(target_os = "linux", target_os = "macos"))]
                 static LINKME_START: <#ty as linkme::private::Slice>::Element;
 
                 #[cfg_attr(target_os = "linux", link_name = #linux_section_stop)]
                 #[cfg_attr(target_os = "macos", link_name = #macos_section_stop)]
-                #[cfg(any(target_os = "linux", target_os = "macos"))]
                 static LINKME_STOP: <#ty as linkme::private::Slice>::Element;
             }
 
-            #[cfg_attr(target_os = "windows", link_section = #windows_section_start)]
             #[cfg(target_os = "windows")]
+            #[link_section = #windows_section_start]
             static LINKME_START: () = ();
 
-            #[cfg_attr(target_os = "windows", link_section = #windows_section_stop)]
             #[cfg(target_os = "windows")]
+            #[link_section = #windows_section_stop]
             static LINKME_STOP: () = ();
 
-            #[cfg_attr(target_os = "linux", link_section = #linux_section)]
             #[cfg(target_os = "linux")]
+            #[link_section = #linux_section]
             static LINKME_PLEASE: [<#ty as linkme::private::Slice>::Element; 0] = [];
 
             #[cfg(not(any(target_os = "linux", target_os = "macos", target_os = "windows")))]
