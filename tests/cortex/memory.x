37adf9b475cd7d34a6e266b6db5f6e6195df3595
David Tolnay <dtolnay@gmail.com>
2019-08-26 10:17:03 -0700

    Add no_std example
    From arcnmx/cortex-m-linkme-example.

---
+++ tests/cortex/memory.x
@@ -0,0 +1,11 @@
+MEMORY
+{
+  FLASH : ORIGIN = 0x00000000, LENGTH = 256K
+  RAM : ORIGIN = 0x20000000, LENGTH = 64K
+}
+
+SECTIONS {
+  linkme_SHENANIGANS : { *(linkme_SHENANIGANS) } > FLASH
+  linkme_EMPTY : { *(linkme_EMPTY) } > FLASH
+}
+INSERT BEFORE .rodata
---
+++ tests/cortex/src/main.rs
@@ -0,0 +1,45 @@
+#![no_main]
+#![no_std]
+
+extern crate panic_semihosting;
+
+use cortex_m_rt::entry;
+use cortex_m_semihosting::{debug, hprintln};
+
+use linkme::distributed_slice;
+
+#[distributed_slice]
+static SHENANIGANS: [i32] = [..];
+
+#[distributed_slice(SHENANIGANS)]
+static N: i32 = 9;
+
+#[distributed_slice(SHENANIGANS)]
+static NN: i32 = 99;
+
+#[distributed_slice(SHENANIGANS)]
+static NNN: i32 = 999;
+
+#[entry]
+fn main() -> ! {
+    assert_eq!(SHENANIGANS.len(), 3);
+
+    let mut sum = 0;
+    for n in SHENANIGANS {
+        sum += n;
+    }
+
+    assert_eq!(sum, 9 + 99 + 999);
+
+    #[distributed_slice]
+    static EMPTY: [i32] = [..];
+
+    assert!(EMPTY.is_empty());
+
+    hprintln!("success!").unwrap();
+
+    // exit QEMU
+    debug::exit(debug::EXIT_SUCCESS);
+
+    loop {}
+}
