637b82e6391891d9bfa3c5f017912e2735f699d9
David Tolnay <dtolnay@gmail.com>
2021-05-13 13:29:08 -0700

    Combine the two custom linkme path tests

--- tests/custom_linkme_path.rs
+++ tests/custom_linkme_path.rs
@@ -8,8 +8,17 @@ mod declaration {
     pub static SLICE: [i32] = [..];
 
     #[test]
-    fn test_mod_slice() {
+    fn test_slice() {
         assert!(!SLICE.is_empty());
+    }
+
+    #[distributed_slice]
+    #[linkme(crate = crate::link_me)]
+    pub static FUNCTIONS: [fn()] = [..];
+
+    #[test]
+    fn test_functions() {
+        assert!(!FUNCTIONS.is_empty());
     }
 }
 
@@ -19,4 +28,8 @@ mod usage {
     #[distributed_slice(super::declaration::SLICE)]
     #[linkme(crate = crate::link_me)]
     pub static N: i32 = 9;
+
+    #[distributed_slice(super::declaration::FUNCTIONS)]
+    #[linkme(crate = crate::link_me)]
+    fn test_me() {}
 }
