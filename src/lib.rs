9e97b038c083e82abae7a1cc061e1b98a6b52dd6
David Tolnay <dtolnay@gmail.com>
2022-11-29 12:33:54 -0800

    Release 0.3.6

--- src/lib.rs
+++ src/lib.rs
@@ -106,7 +106,7 @@
 //! ```
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/linkme/0.3.5")]
+#![doc(html_root_url = "https://docs.rs/linkme/0.3.6")]
 #![allow(
     clippy::doc_markdown,
     clippy::empty_enum,
