b80482897192ef3f161e2168d5439b189e86f0fb
David Tolnay <dtolnay@gmail.com>
2023-07-20 21:44:11 -0700

    Release 0.3.13

--- src/lib.rs
+++ src/lib.rs
@@ -106,7 +106,7 @@
 //! ```
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/linkme/0.3.12")]
+#![doc(html_root_url = "https://docs.rs/linkme/0.3.13")]
 #![allow(
     clippy::doc_markdown,
     clippy::empty_enum,
