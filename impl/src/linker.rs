36abe6b20daef0905574c2944ef98f4b04b237a1
David Tolnay <dtolnay@gmail.com>
2024-06-05 11:43:25 -0700

    Combine some freebsd and openbsd cfgs

--- impl/src/linker.rs
+++ impl/src/linker.rs
@@ -14,23 +14,7 @@ pub mod linux {
     }
 }
 
-pub mod freebsd {
-    use syn::Ident;
-
-    pub fn section(ident: &Ident) -> String {
-        format!("linkme_{}", ident)
-    }
-
-    pub fn section_start(ident: &Ident) -> String {
-        format!("__start_linkme_{}", ident)
-    }
-
-    pub fn section_stop(ident: &Ident) -> String {
-        format!("__stop_linkme_{}", ident)
-    }
-}
-
-pub mod openbsd {
+pub mod bsd {
     use syn::Ident;
 
     pub fn section(ident: &Ident) -> String {
