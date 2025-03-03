ee2a7d4ae6d1e0f34f717a8e6e8321a566906aec
David Tolnay <dtolnay@gmail.com>
2025-03-02 23:54:25 -0800

    Release 0.3.32

--- src/lib.rs
+++ src/lib.rs
@@ -134,7 +134,7 @@
 //! </details>
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/linkme/0.3.31")]
+#![doc(html_root_url = "https://docs.rs/linkme/0.3.32")]
 #![deny(unsafe_op_in_unsafe_fn)]
 #![allow(
     clippy::doc_markdown,
